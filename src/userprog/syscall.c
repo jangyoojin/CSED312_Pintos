146973419adf087081ad8ed5824121994c8c40fe
eunhaPark <dmsgk724@postech.ac.kr>
2022-11-04 00:30:15 +0900

    syscall handler

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -4,6 +4,9 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
+#define STACK_END 0x8048000
+#define STACK_BASE 0xc0000000
+
 static void syscall_handler (struct intr_frame *);
 
 void
@@ -15,6 +18,93 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+  int argv[3];
+  uint32_t *sp= f->esp;
+  check_user_addr((void*)sp);
+  int syscall_number=*sp;
+  
+  switch(syscall_number)
+  {
+    case SYS_HALT:
+      halt();
+      break;
+    case SYS_EXIT:
+      get_arg(sp,argv,1);
+      exit(argv[0]);
+      break;
+    case SYS_EXEC:
+      get_arg(sp, argv,1);
+      f->eax=exec((const char *) argv[0]);
+      break;
+    case SYS_WAIT:
+      get_arg(sp,argv,1);
+      f->eax=wait((pid_t)argv[0]);
+      break;
+    case SYS_CREATE:
+      get_arg(sp,argv,2);
+      f->eax=create_file(argv[0],argv[1]);
+      break;
+    case SYS_REMOVE:
+      get_arg(sp,argv,1);
+      f->eax=remove_file(argv[0]);
+      break;
+    case SYS_OPEN:
+      get_arg(sp,argv,1);
+      f->eax=open(argv[0]);
+      break;
+    case SYS_FILESIZE:
+      get_arg(sp,argv,1);
+      f->eax=filesize(argv[0]);
+      break;
+    case SYS_READ :
+      get_arg(sp,argv,3);
+      f->eax=read(argv[0],argv[1],argv[2]);
+      break;
+    case SYS_WRITE:
+      get_arg(sp,argv,3);
+      f->eax=write(argv[0],argv[1],argv[2]);
+      break;
+    case SYS_SEEK:
+      get_arg(sp,argv,2);
+      seek(argv[0],argv[1]);
+      break;
+    case SYS_TELL:
+      get_arg(sp,argv,1);
+      f->eax=tell(argv[0]);
+      break;
+    case SYS_CLOSE:
+      get_arg(sp,argv,1);
+      close(argv[0]);
+      break;
+
+      
+
+    
+  }
+
+  }
+
+
+
+
+
+
+
+
+
+
+void check_user_addr(void *addr)
+{
+if(addr<STACK_END && addr>=STACK_BASE) exit(-1);
+}
+
+void get_arg(int *esp, int * argv, int argc)
+{
+  int i;
+  for(i=0;i<argc;i++)
+  {
+    check_user_addr(esp);
+    esp+=4;
+    argv[i]=*esp;
+  }
 }
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,27 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+typedef int pid_t;
+struct lock filesys_lock;
+
+
 void syscall_init (void);
+void check_user_addr(void *addr);
+void get_arg(int * esp,int *argv, int argc);
+void halt();
+void exit(int status);
+bool create_file(const char *file, unsigned initial_size);
+bool remove_file(const char *file);
+pid_t exec(const char * cmdline);
+int wait (tid_t tid);
+int open(const char *file);
+int filesize(int fd);
+int read (int fd, void *buffer, unsigned size);
+int write(int fd, void *buffer, unsigned size);
+void seek (int fd, unsigned position);
+unsigned tell (int fd);
+void close (int fd);
+
+
 
 #endif /* userprog/syscall.h */
