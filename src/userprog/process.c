a532864c944e63cc136600dde2cc1ca4292c49aa
eunhaPark <dmsgk724@postech.ac.kr>
2022-11-19 19:58:12 +0900

    34 test failed

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -125,7 +125,6 @@ start_process (void *file_name_)
   
   palloc_free_page(argv);
   palloc_free_page(file_name_copy);
-  //hex_dump(if_.esp,if_.esp,PHYS_BASE-if_.esp,true);
 
   /* Start the user process by simulating a return from an
      interrupt, implemented by intr_exit (in
